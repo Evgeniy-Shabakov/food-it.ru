a7a8ec4a2128ec10640b09d91a835104e07086db
Evgeniy-Shabakov <shabakovevgeniy@gmail.com>
2025-06-13 18:02:17 +0300

    Section Demonstration - CSS changing

--- components/page/index/sectionDemonstration.vue
+++ components/page/index/sectionDemonstration.vue
@@ -1,39 +1,39 @@
 <template>
 
-    <section>
+   <section>
 
-        <TitleH2 class="mb-10">
-            Демонстрационная версия
-        </TitleH2>
+      <TitleH2 class="mb-10">
+         Демонстрационная версия
+      </TitleH2>
 
-        <div class="flex flex-col gap-8 items-center md:flex-row md:justify-center md:gap-14">
+      <div class="flex flex-col gap-8 md:flex-row md:justify-center md:gap-14">
 
-            <CardDemonstration link="https://demopizza.ru">
-                <template v-slot:icon>
-                    <IconGooglePlay class="text-green-500" />
-                </template>
-                <template v-slot:h3>
-                    Мобильное приложение
-                </template>
-                <template v-slot:p>
-                    Приложение для Android
-                </template>
-            </CardDemonstration>
+         <CardDemonstration link="https://demopizza.ru">
+            <template v-slot:icon>
+               <IconGooglePlay class="text-green-500" />
+            </template>
+            <template v-slot:h3>
+               Мобильное приложение
+            </template>
+            <template v-slot:p>
+               Приложение для Android (ссылка будет позже)
+            </template>
+         </CardDemonstration>
 
-            <CardDemonstration link="https://demopizza.ru">
-                <template v-slot:icon>
-                    <IconInternet class="text-blue-500" />
-                </template>
-                <template v-slot:h3>
-                    Сайт
-                </template>
-                <template v-slot:p>
-                    Веб-сайт адаптированный под все типы устройств
-                </template>
-            </CardDemonstration>
+         <CardDemonstration link="https://demopizza.ru">
+            <template v-slot:icon>
+               <IconInternet class="text-blue-500" />
+            </template>
+            <template v-slot:h3>
+               Сайт
+            </template>
+            <template v-slot:p>
+               Веб-сайт адаптированный под все типы устройств
+            </template>
+         </CardDemonstration>
 
-        </div>
+      </div>
 
-    </section>
-    
+   </section>
+
 </template>
